2e3c60376bc817cf8fb508ba978574767a0222ee
Ayush Shete <ayushete02@gmail.com>
2023-03-24 18:26:08 +0530

    Push Protocol Chatting box added

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { NavLink  } from "react-router-dom";
 import Herosection from './Herosection';
 import Navbar from './Navbar';
@@ -6,16 +5,63 @@ import Screenshot from './Screenshot';
 import ProblemSolve from './ProblemSolve';
 import Footer from './Footer';
 import WorkingProcess from './WorkingProcess';
+import * as PushAPI from "@pushprotocol/restapi";
+import React, { useState, useEffect } from "react";
+import { Button, FloatButton,Popover, Card, Drawer } from 'antd';
+import { Chat } from "@pushprotocol/uiweb";
 
 const LandingPage = () => {
+
+  const [open, setOpen] = useState(false);
+
+  const [notification, SetNotification] = useState([]);
+  const NotificationReceiver = async () => {
+    const notifications = await PushAPI.user.getFeeds({
+      user: "eip155:5:0x7ED790A1Ac108b9A50e24f5c5E061df59e3673a7", // user address in CAIP
+      env: "staging",
+    });
+    SetNotification(notifications);
+  };
+
+  const showDrawer = () => {
+    NotificationReceiver();
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setOpen(false);
+  };
   return (
-    <div className='bg-gradient-to-br from-[#170E5F] via-blue-900 to-[#050509]'>
+    <div className='bg-gradient-to-r from-[#ffffff] via-blue-900 to-[#050509]'>
      <Navbar/>
      <Herosection/>
      <Screenshot/>
      <ProblemSolve/>
      <WorkingProcess/>
      <Footer/>
+
+     <Drawer  title="Push Notification ({0x7ED790A1Ac108b9A50e24f5c5E061df59e3673a7})" width={'700px'} placement="right" onClose={onClose} open={open}>
+
+          {notification.map((item, index) => {
+            console.log(notification);
+            return (
+            
+              <Card key='key' className="mt-3" type="inner" title={item['app']} extra={<a href="#"><b>{item['blockchain']}</b></a>}>
+                {item['message']}
+              </Card>
+      
+            );
+          })}
+      </Drawer>
+
+     {/* <Chat className='z-50'
+        // account={address} //user address
+        account='0x7ED790A1Ac108b9A50e24f5c5E061df59e3673a7' //user address
+        supportAddress="0xC76139fcB9e4952CE9Fb3183C6c3af69534233FE" //support address
+        apiKey={process.env.NEXT_PUBLIC_PUSH_API}
+        env="staging"
+        modalTitle='Support team'
+      /> */}
     </div>
   );
 }
--- src/components/LandingPage/Screenshot.tsx
+++ src/components/LandingPage/Screenshot.tsx
@@ -5,8 +5,8 @@ const Screenshot = () => {
   return (
     <div className=''>
 
-    <div className='w-[80%] mx-auto -mt-36 shadow-lg bg-[#1f1f1f]'>
-        <div className=' pt-8 rounded-lg bg-white border-red border-2'>
+    <div className='w-[80%] mx-auto -mt-36 shadow-lg bg-[#1f1f1f] bg-transparent'>
+        <div className=' pt-8 rounded-lg bg-white border-red '>
       <img src={img} alt="" />
 
         </div>
